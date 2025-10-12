6434f7105072aba1dca8e9507694b32a01c1ccd4
disindu123 <disindudamsandu@gmail.com>
2025-10-12 20:20:58 +0530

    Update

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=0704158ac0cdb0b8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=sQFx1I5K#5xb-A4DE4ydr8V-xB6O5dUxkrQkq_kkVqzTds7gb9AM' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
